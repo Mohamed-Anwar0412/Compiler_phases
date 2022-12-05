de8f8e289788e287830460e06628b5587b57b7bc
Bboy-Anwar <106599001+Mohamed-Anwar0412@users.noreply.github.com>
2022-12-06 00:29:36 +0200

    llparser

--- Syntax_Analyzer.py
+++ Syntax_Analyzer.py
@@ -1,5 +1,6 @@
-import tokenize as tn
 import re
+import numpy as np
+from tabulate import tabulate  # table the data
 
 reject = False
 tokens = []
@@ -7,6 +8,8 @@ nonTerm =[]
 term = []
 inputStack =[]
 parserStack = ['S']
+Data = np.empty((1, 1))
+i= 0
 
 def setParser(lX, cfg):
     global tokens, nonTerm, term, inputStack
@@ -16,24 +19,112 @@ def setParser(lX, cfg):
     for token in tokens:
         inputStack.append(token.string)
     print(inputStack)
-    #inputStack.reverse()
+    inputStack.reverse()
+    #print(inputStack)
 
 
     # def match(self, key, s):
     #     return re.match(self.Term[key], s)
 
+def checkTerm(key):
+    if key in [key for key in term]:
+        return True
+    return False
+
+def stackMatch():
+    global Data, parserStack, inputStack, i
+    key = parserStack[-1]
+    parserStack.pop()
+    parserStack.append(inputStack[-1])
+    Data[-1][-1] = "Predict: {} --> {}".format(key, inputStack[-1])
+    Data = np.append(Data, [parserStack, inputStack, 'a'])
+    i += 1
+    inputStack.pop()
+    parserStack.pop()
+    Data = np.append(Data, [parserStack, inputStack, 'a'])
+    i += 1
+
 def LLparser():
-    global reject
-    while True:
+    global reject, Data, parserStack, inputStack, i
+    Data = np.append(Data, [parserStack, inputStack, 'a'])
+    i += 1
+    while not reject:
         if inputStack == [] and parserStack == []:
             break;
-        if parserStack[0] in [key for key in term]:
-            if re.match(term[parserStack[0]], inputStack[-1]):
-                inputStack.pop(0)
-                parserStack.pop(0)
+        if parserStack[-1] == inputStack[-1]:
+            Data[-1][-1] = "Match {}".format(inputStack[-1])
+            inputStack.pop()
+            parserStack.pop()
+            Data = np.append(Data, [parserStack, inputStack, 'a'])
+            i += 1
+        elif checkTerm(parserStack[-1]):
+            if re.match(term[parserStack[-1]], inputStack[-1]):
+                stackMatch()
             else:
+                Data[-1][-1] = 'Reject!!'
                 reject = True
-                break
+                break;
+        elif isinstance(nonTerm[parserStack[-1]], str):
+            string = nonTerm[parserStack[-1]].split(' ')
+            string.reverse()
+            Data[-1][-1] = 'Predict: {key} --> {value}'.format(key = parserStack[-1], value = nonTerm[parserStack[-1]])
+            parserStack.pop()
+            for letter in string:
+                parserStack.append(letter)
+            Data = np.append(Data, [parserStack, inputStack, 'a'])
+            i += 1
+        else:
+            choice = decider(parserStack[-1], inputStack[-1])
+            if choice == -1:
+                Data[-1][-1] = 'Predict: {} --> λ'.format(parserStack[-1])
+                parserStack.pop()
+                Data = np.append(Data, [parserStack, inputStack, 'a'])
+                i += 1
+            elif choice == -2:
+                Data[-1][-1] = 'Reject!!'
+                reject = True
+                break;
+            else:
+                string = nonTerm[parserStack[-1]][choice].split(' ')
+                string.reverse()
+                Data[-1][-1] = 'Predict: {key} --> {value}'.format(key = parserStack[-1], value = nonTerm[parserStack[-1]][choice])
+                parserStack.pop()
+                for letter in string:
+                    parserStack.append(letter)
+                Data = np.append(Data, [parserStack, inputStack, 'a'])
+                i += 1
 
 
+def decider(key, currentToken):
+    index = 0
+    for choice in nonTerm[key]:
+        choice = choice.split(' ')
+        if deciderParser(choice[0], currentToken):
+            return index
+        index += 1
+    if nonTerm[key][-1] == '':
+        return -1
+    return -2
 
+
+def deciderParser(key, currentToken):
+    if key == currentToken:
+        return True
+    elif checkTerm(key):
+        return re.match(term[key], currentToken)
+    elif key in [keys for keys in nonTerm]:
+        if isinstance(nonTerm[key], str):
+            string = nonTerm[key].split(' ')
+            return deciderParser(string[0], currentToken)
+        else:
+            choice = decider(key, currentToken)
+            if choice == -1 or choice == -2:
+                return False
+            else:
+                return True
+
+def mapping():
+    global Data, i
+    Data.shape = (i, 3)
+    table = tabulate(Data, headers=['parser Stack', 'input Stack', 'Action'])
+    return table
--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import Lexical_Analyzer as lX
 import CFG
 import Syntax_Analyzer as sA
 import re
+import ply.yacc as yacc
+import nltk
+#nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+nltk.download('treebank')
 
 URL = 'test.txt'
 
@@ -20,6 +27,11 @@ URL = 'test.txt'
 _lexAnalyzer = lX.LexicalAnalyzer(URL)
 cfg_rules = CFG.CFG()
 sA.setParser(_lexAnalyzer,cfg_rules)
+sA.LLparser()
+sA.mapping()
+string = ['sdf', 'asdfas', 'sadf']
+string.reverse()
+print(string)
 #print(cfg1.cfg)
 
 # print(sA.tokens)
@@ -34,4 +46,32 @@ sA.setParser(_lexAnalyzer,cfg_rules)
 #     print('a7a')
 
 
-#print(_lexAnalyzer.mapping())
+# #print(_lexAnalyzer.mapping())
+# f = open(URL)
+# st = f.read()
+# print(st)
+# tokens = nltk.word_tokenize(st)
+# print(tokens)
+#
+# tagged = nltk.pos_tag(tokens)
+# print(tagged)
+#
+# entities = nltk.chunk.ne_chunk(tagged)
+# print(entities)
+# # chunker = nltk.RegexpParser()
+# # from nltk.corpus import treebank
+# # t = treebank.parsed_sents(tokens)[0]
+# # t.draw()
+# nonterminals = [key for key in cfg_rules.NonTerm]
+# combinded = ''
+# for i in nonterminals:
+#     combinded += i
+#     combinded +=' '
+# # combinded = combinded[:-2]
+# print(combinded)
+# nltk.grammar.ter
+# print(nltk.grammar.nonterminals(combinded))
+
+# if isinstance(cfg_rules.NonTerm['T'], list):
+#     print('sdf')
+# print(sA.nt_Decider('T', 's'))
